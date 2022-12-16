8fd30dcd26a4dee6946257cdef02c5e7191eb56c
Lucas Fuentes <lucasfuentes100@hotmail.com>
2022-12-15 18:35:43 -0800

    implemented resume-from-preexisting, skip fwd/back without overwriting, and skip fwd/back arbitrary amount

--- annotator.py
+++ annotator.py
@@ -13,7 +13,9 @@ import os
 path_to_images = "./output"
 image_type = "png"
 path_to_spot_list_csv = "./spot_list_ilastik_U3D.csv"
-path_to_out_csv = "./annotated_spot_list_ilastik_U3D.csv"
+path_to_out_csv = "./new_annotated_spot_list_ilastik_U3D.csv"
+
+overwrite_pre = False # set True to overwrite the previous path_to_out
 
 fix_number = True # set True if the view numbers are offset by 100
 
@@ -25,36 +27,38 @@ save_interval = 10 # write the CSV every 10 annotations
 pygame.init()
 scrn = pygame.display.set_mode((sz*3,sz))
 
-# Load spot list CSVg
-spotlist = pd.read_csv(path_to_spot_list_csv)
-n_imgs = spotlist.shape[0]
-print(n_imgs)
+# Load spot list CSV
+# if it already exists and we don't want to overwrite, load it
+if os.path.exists(path_to_out_csv) and not overwrite_pre:
+    spotlist = pd.read_csv(path_to_out_csv)
+# otherwise, remake it.
+else:
+    spotlist = pd.read_csv(path_to_spot_list_csv)
+    n_imgs = spotlist.shape[0]
+    # add annotations column, assign default value
+    spotlist["annotations"] = "-1"
+    # fix numbering
+    if fix_number:
+        spotlist["FOV_row"] = spotlist["FOV_row"]  - 100
+        spotlist["FOV_col"] = spotlist["FOV_col"]  - 100
+    spotlist.to_csv(path_to_out_csv, mode='w', index=False)
 
+n_imgs = spotlist.shape[0]
+# randomize order
 indices = list(range(n_imgs))
 if random_order:
     random.shuffle(indices)
 
-index = 0
-column_names = ["annotation"] + list(spotlist.columns)
-classifications = pd.DataFrame(columns = column_names)
-# write headers, create CSV
-classifications.to_csv(path_to_out_csv, mode='w', index=False)
-
 # loop over images
 i = 0
-j = 0
 while i < n_imgs:
     index = indices[i]
     # get the image path
-    if fix_number:
-        img_name = str(int(spotlist["FOV_row"][index])-100)  + "_" + str(int(spotlist["FOV_col"][index])-100) + "_" + str(spotlist["x"][index]) + "_" + str(spotlist["y"][index])
-    else:
-        img_name = str(spotlist["FOV_row"][index])  + "_" + str(spotlist["FOV_col"][index]) + "_" + str(spotlist["x"][index]) + "_" + str(spotlist["y"][index])
-
+    img_name = str(int(spotlist["FOV_row"][index]))  + "_" + str(int(spotlist["FOV_col"][index])) + "_" + str(int(spotlist["x"][index])) + "_" + str(int(spotlist["y"][index]))
     target_path = os.path.join(path_to_images, img_name + "." + image_type)
     print(target_path)
     # set window name
-    pygame.display.set_caption(img_name)
+    pygame.display.set_caption(img_name + ": " + str(spotlist["annotations"][index]))
     # open the image and resize it
     imp = pygame.image.load(target_path).convert()
     imp = pygame.transform.scale(imp, (sz*3,sz))
@@ -82,21 +86,15 @@ while i < n_imgs:
     print(key_pressed)
     status = -1
     if key_pressed == 'a':
-        # case a - go back to the previous image
-        # and delete the previous row
+        # case a - go back to the previous image without modifying current annotation
         i -= 1
-        try:
-            classifications.drop(classifications.index[[len(classifications)-1]], inplace=True)
-        except:
-            # If we can't drop, it's because there's nothing left to drop. We can't go back!
-            print("Already at end!")
-            i += 1
-            pass
+        status = spotlist["annotations"][index]
         i = max(0, i)
-    # if key_pressed == 'g':
-    #     # case g - go to next image
-    #     i += 1
-    #     i = min(n_imgs, i)
+    if key_pressed == 'g':
+        # case g - go to next image without modifying current annotation
+        i += 1
+        status = spotlist["annotations"][index]
+        i = min(n_imgs, i)
     if key_pressed == 's':
         # case s: mark as positive and go to next
         status = 1
@@ -110,15 +108,13 @@ while i < n_imgs:
         status = 9
         i += 1
 
-    # append row to dataframe
-    if status != -1:
-        classifications.loc[len(classifications)] = [str(status)] + list(spotlist.iloc[index])
-    print(classifications)
+    # add data to dataframe
+    if key_pressed != 'q':
+        spotlist.loc[index, 'annotations'] = str(status)
+        print(spotlist.loc[index])
     # save the CSV
-    if (len(classifications) % save_interval) == 0 or key_pressed == 'q':
-        classifications.to_csv(path_to_out_csv, mode='a', index=False, header=False)
-        # reset classifications
-        classifications = pd.DataFrame(columns = column_names)
+    if ((i+1)% save_interval) == 0 or key_pressed == 'q':
+        spotlist.to_csv(path_to_out_csv, mode='w', index=False)
 
     if key_pressed == 'q':
         break
